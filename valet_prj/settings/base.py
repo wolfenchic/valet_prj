2ffc9507b13aa73c165c549d87369c547ea5eb23
Nichola Wolfe <wolfe.nichola@gmail.com>
2018-06-03 11:14:50 +0000

    Split settings files

--- valet_prj/settings/base.py
+++ valet_prj/settings/base.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -80,11 +81,9 @@ WSGI_APPLICATION = 'valet_prj.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL", "sqlite:///db.sqlite3"))
+     }
+
 
 
 # Password validation
@@ -127,6 +126,11 @@ STATIC_URL = '/static/'
 
 MESSAGE_STORAGE='django.contrib.messages.storage.session.SessionStorage'
 
-STRIPE_PUBLISHABLE_KEY = "pk_test_0YXsjzjSRhNvVRKvnUJeavXh"
-STRIPE_SECRET = "sk_test_OPAli4itejK7cGtUSsnV0CkH"
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'accounts.backends.EmailAuth'
+]
+
+STRIPE_PUBLISHABLE_KEY = os.getenv('STRIPE_PUBLISHABLE_KEY')
+STRIPE_SECRET = os.getenv('STRIPE_SECRET_KEY')
 
