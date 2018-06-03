491c82a521054c34277bc341ffe880fd9e9af621
Nichola Wolfe <wolfe.nichola@gmail.com>
2018-06-03 11:27:10 +0000

    Added Proc

--- valet_prj/settings/base.py
+++ valet_prj/settings/base.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'vwybkh(%44t9_8c-t3bexbfm%qp5xo5!+k$#)4esh3gn#b!xky'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
