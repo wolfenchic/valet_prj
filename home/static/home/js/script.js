64601572eed2ce647ac0d12a5f07f4f3edb2a886
Nichola Wolfe <wolfe.nichola@gmail.com>
2018-06-04 19:30:36 +0000

    Added services

--- home/static/home/js/script.js
+++ home/static/home/js/script.js
@@ -1,37 +1,12 @@
 $(document).ready(function(){
 
 $('a').mouseenter(function() {
- 	$(this).css("color", "#C3AEAF");
+ 	$(this).css("color", " #EB8732");
  });
 
 $('a').mouseleave(function(){
     $(this).css("color", "#4a4a4f");
 });
-
-$(".card").mouseenter(function(){
-    $(this).animate({opacity: '1.0'}, "slow");
-});
-
-$(".card").mouseleave(function(){
-    $(this).animate({opacity: '0.6'}, "slow");
-});
-
-$(".fa-linkedin, .fa-github, .fa-file").mouseenter(function(){
-    $(this).css("color", "#4a4a4f");
-});
-
-$(".fa-linkedin, .fa-github, .fa-file").mouseleave(function(){
-    $(this).css("color", "#C3AEAF");
-});
-
-$(".contactButton").mouseenter(function(){
-    $(this).css("background-color", "#EA615B");
-});
-
-$(".contactButton").mouseleave(function(){
-    $(this).css("background-color", "#EA615B");
-});
-
 
 
 });
